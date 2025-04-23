5bc39d932c06dc0e432d10899986271f3a53e4bd
Teseo Nicholson <teseonicholson@gmail.com>
2025-04-23 14:14:10 +0100

    Implement POST request to submit form data for JSHint checks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,21 @@ const resultsModal = new bootstrap.Modal(
 );
 
 document
-  .getElementById("status").addEventListener("click", (e) => getStatus(e));
+  .getElementById("status")
+  .addEventListener("click", (e) => getStatus(e));
+document.getElementById("submit").addEventListener("click", (e) => postForm(e));
+
+async function postForm(e) {
+  const form = new FormData(document.getElementById("checksform"));
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: API_KEY,
+    },
+    body: form,
+  });
+}
 
 async function getStatus(e) {
   const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -19,10 +33,10 @@ async function getStatus(e) {
 }
 
 function displayStatus(data) {
-    let heading = "API Key Status";
-    let results = `<div>Your key is valid until</div>`;
-    results += `<div class="key-status">${data.expiry}</div>`;
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
+  let heading = "API Key Status";
+  let results = `<div>Your key is valid until</div>`;
+  results += `<div class="key-status">${data.expiry}</div>`;
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
+  resultsModal.show();
 }
