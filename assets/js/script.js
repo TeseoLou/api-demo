fc59b9ffa5d30a33c5683464d5599c549c7b302f
Teseo Nicholson <teseonicholson@gmail.com>
2025-04-23 14:04:07 +0100

    Add API error handling and display key expiry in Bootstrap modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
 const API_KEY = "cUIA8c5CkQLhWyi16tNhWPToRK4";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const reultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+const resultsModal = new bootstrap.Modal(
+  document.getElementById("resultsModal")
+);
 
-document.getElementById("status").addEventListener("click", e => getStatus(e));
+document
+  .getElementById("status").addEventListener("click", (e) => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
-    const response = await fetch(queryString);
-    const data = await response.json();
-    if (response.ok) {
-        console.log(data.expiry);
-    }
+  const queryString = `${API_URL}?api_key=${API_KEY}`;
+  const response = await fetch(queryString);
+  const data = await response.json();
+  if (response.ok) {
+    displayStatus(data);
+  } else {
+    throw new Error(data.error);
+  }
 }
-https://ci-jshint.herokuapp.com/api?api_key=cUIA8c5CkQLhWyi16tNhWPToRK4
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
